57f1be88202274318c15286371b8d975f27835ba
PedroPumada <PedroPumada>
2014-02-27 20:07:14 -0500

    Added elseif to ifstatement.php

--- classwork/week1/ifstatement.php
+++ classwork/week1/ifstatement.php
@@ -7,10 +7,12 @@
     <p>Complete the if statement</p>
     <p>
       <?php
-        $age =  12;    // Set this to a number greater than 18
+        $age =  16;    // Set this to a number greater than 18
 
         if ($age > 18) {
           echo "You're old enough to vote!";
+        } elseif ($age==16) {
+          echo "Happy Sweet 16! (If you're a girl) But you still can't vote.";
         } else {
           echo "You are not old enough to vote. I hope you've at least hit puberty.";
         }

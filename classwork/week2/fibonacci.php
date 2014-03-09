cfd1074e56b5171f37fe461fd8835ffad6139dbc
PedroPumada <pgzink@gmail.com>
2014-03-09 13:29:37 -0400

    Solved fibonacci.php

--- classwork/week2/fibonacci.php
+++ classwork/week2/fibonacci.php
@@ -9,14 +9,15 @@
       // Create a function that takes a number $n
       // if the number $n is equal to 1 or 2 then return 1
       // else return the function itself to proceed to the next fibonacci number
+
     function fibonacci ($n) {
-      if ($n == 1 OR $n == 2) {
+      if ($n == 1 || $n == 2) {
         return 1;
       } else {
-        return fibonacci($n);
+        return (fibonacci($n - 1) + fibonacci($n -2));
       }
     }
-    echo '<p>' . fibonacci(3); '</p>';
+    echo '<p>' . fibonacci(5) . '</p>';
     ?>
   </body>
 </html>
